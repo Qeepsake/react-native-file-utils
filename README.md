00ad6fe1e912c8bf935e93e85ccc3779be35f8bc
Thomas Brian <tdbrian@gmail.com>
2021-12-13 14:21:21 -0600

    Add functions in TS and update readme

--- README.md
+++ README.md
@@ -12,12 +12,44 @@ npm install @qeepsake/react-native-file-utils
 
 ## Usage
 
+### Get video file duration
+
+Gets the duration in milliseconds of the video at the file path on the device that's passed.
+
 ```js
-import { multiply } from '@qeepsake/react-native-file-utils';
+import { getDuration } from '@qeepsake/react-native-file-utils';
 
-// ...
+const durationMs = await getDuration('file://<media-path>', 'video');
+```
 
-const result = await multiply(3, 7);
+### Get media file's dimensions in pixels
+
+Gets the horizontal (x) and vertical (y) pixels of the media item, either image or video. The returned media dimensions includes both the horizontal (x) length in pixels and vertical (y) length in pixels.
+
+```js
+import { getDimensions } from '@qeepsake/react-native-file-utils';
+
+const mediaDimensions = await getDimensions('file://<media-path>', 'video');
+```
+
+### Get media file mime type
+
+Gets the Mime type of the media file at the passed Uri.
+
+```js
+import { getMimeType } from '@qeepsake/react-native-file-utils';
+
+const mimeType = await getMimeType('file://<media-path>');
+```
+
+### Get timestamp on the media item
+
+Gets the timestamp (js Date) of the media file at the passed Uri.
+
+```js
+import { getTimestamp } from '@qeepsake/react-native-file-utils';
+
+const timestamp = await getTimestamp('file://<media-path>');
 ```
 
 ## Contributing
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import { NativeModules, Platform } from 'react-native';
+import type { MediaSize } from './types/MediaSize';
 
 const LINKING_ERROR =
-  `The package 'react-native-file-utils' doesn't seem to be linked. Make sure: \n\n` +
+  `The package '@qeepsake/react-native-file-utils' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
@@ -17,6 +18,46 @@ const FileUtils = NativeModules.FileUtils
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return FileUtils.multiply(a, b);
+/**
+ * Gets the duration of the media item, either image or video, in milliseconds.
+ * @param uri The full on device uri for the media item.
+ * @param mediaType - Either 'image' or 'video. If passing an image, 0 will always be returned.
+ * @returns
+ */
+export function getDuration(
+  uri: string,
+  mediaType: 'video' | 'image'
+): Promise<number> {
+  return FileUtils.getDuration(uri, mediaType);
+}
+
+/**
+ * Gets the horizontal (x) and vertical (y) pixels of the media item, either image or video.
+ * @param uri The full on device uri for the media item.
+ * @param mediaType - Either 'image' or 'video. If passing an image, 0 will always be returned.
+ * @returns MediaSize with height and width of the media item in pixels.
+ */
+export function getDimensions(
+  uri: string,
+  mediaType: 'video' | 'image'
+): Promise<MediaSize> {
+  return FileUtils.getDimensions(uri, mediaType);
+}
+
+/**
+ * Gets the Mime type of the media file at the passed Uri.
+ * @param uri The full on device uri for the media item.
+ * @returns The Mime type of the media file.
+ */
+export function getMimeType(uri: string): Promise<string> {
+  return FileUtils.getMimeType(uri);
+}
+
+/**
+ * Gets the timestamp of the media file at the passed Uri.
+ * @param uri The full on device uri for the media item.
+ * @returns The timestamp of the media file.
+ */
+export function getTimestamp(uri: string): Promise<Date> {
+  return FileUtils.getTimestamp(uri);
 }
